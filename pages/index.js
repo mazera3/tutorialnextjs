90786ccabb32e8e7d4fabd0727f19dc4eb9e1957
Edio.Mazera <mazera3@gmail.com>
2021-05-28 09:12:52 -0300

    Atualização

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import {
     Alert,
     Container
 } from 'reactstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+// Icones: twitter, coffee, user-graduate
+library.add(fas,fab, faCoffee);
 
 
 const Home = () => (
@@ -19,6 +26,13 @@ const Home = () => (
                 Olá Mundo! </Alert>
             <Button color="danger"> Danger!
             </Button>
+            <div>Icones:<br/>
+                Café: <FontAwesomeIcon icon="coffee" /><br/>
+                Usuario: <FontAwesomeIcon icon="user" /><br/>
+                Graduado: <FontAwesomeIcon icon="user-graduate" /><br/>
+                Twitter: <FontAwesomeIcon icon={['fab', 'twitter']} /><br/>
+                Frasco: <FontAwesomeIcon icon={['fas', 'flask']} /><br/>
+            </div>
         </Container>
     </div>
 );
